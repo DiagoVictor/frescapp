c4b95d81e044ed3c4e5521c25826d06d4cbaed9b
Victor Diago <vmdiagov@gmail.com>
2024-05-09 21:38:45 -0500

    fix email

--- admin/backend/utils/email_utils.py
+++ admin/backend/utils/email_utils.py
@@ -24,10 +24,11 @@ SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 def authenticate():
     # creates credentials with a refresh token
     credential_path = os.path.join(path_file, 'credentials.json')
+    client_secret_path = os.path.join(path_file, 'client_secret.json')
     store = file.Storage(credential_path)
     creds = store.get()
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets(path_file +'client_secret.json', SCOPES)
+        flow = client.flow_from_clientsecrets(client_secret_path, SCOPES)
         creds = tools.run_flow(flow, store)
     return creds
 
