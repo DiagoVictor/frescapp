8c5b7eed3adee5f94e9b8590dc128faf2174662f
Victor Diago <vmdiagov@gmail.com>
2024-05-12 23:19:27 -0500

    fix message

--- admin/backend/api/order_management.py
+++ admin/backend/api/order_management.py
@@ -337,7 +337,8 @@ def send_order_email(order_number, customer_email, delivery_date, products, tota
                                             <td id="header_wrapper"
                                                 style="padding: 36px 48px; display: block; text-text-align: left; padding-top: px; padding-bottom: px; padding-left: 48px; padding-right: 48px;"
                                                 text-align="left">
-                                                <h1>Hemos recibido tu nueva orden, será un gusto entregarla.</h1>
+                                                <h1>Hemos recibido tu nueva orden, </h1><br>
+                                                <h1>será un gusto entregarla. Gracias</h1>
                                             </td>
                                         </tr>
                                     </table>
