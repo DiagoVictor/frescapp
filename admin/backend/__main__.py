4ab20379304ee60e2be5d6c9f45151f766413e8c
Victor Manuel Diago <vmdiagov@gmail.com>
2025-12-19 00:18:34 -0500

    fix ssl

--- admin/backend/__main__.py
+++ admin/backend/__main__.py
@@ -1,4 +1,10 @@
 from backend.app import app
+import os
 
+context = (
+    '/etc/letsencrypt/live/app.buyfrescapp.com/fullchain.pem',
+    '/etc/letsencrypt/live/app.buyfrescapp.com/privkey.pem'
+)
+port = int(os.getenv("PORT", 5000))
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host='0.0.0.0', port=port,  ssl_context=context)
