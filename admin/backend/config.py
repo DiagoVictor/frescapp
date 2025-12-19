54aa5e7f0f71d72f82b53fa027d00c1afebd9432
Victor Manuel Diago <vmdiagov@gmail.com>
2025-12-19 00:00:36 -0500

    mongo uri

--- admin/backend/config.py
+++ admin/backend/config.py
@@ -1,7 +1,7 @@
 import os
 
 # URI local de MongoDB (ajústala según tu entorno)
-MONGO_URI = os.getenv("MONGO_URI")
+MONGO_URI = os.getenv("MONGO_URI_PROD")
 
 # Clave secreta para JWT, sesiones u otras operaciones seguras
 SECRET_KEY = os.getenv("SECRET_KEY")
