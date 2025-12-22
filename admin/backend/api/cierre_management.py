c1959e21388b57002c4077308b78462d52e2bbee
Victor Manuel Diago <vmdiagov@gmail.com>
2025-12-21 22:03:52 -0500

    fix again api

--- admin/backend/api/cierre_management.py
+++ admin/backend/api/cierre_management.py
@@ -12,11 +12,12 @@ from ..models.cost import Cost
 from ..models.order import Order
 from ..models.product import Product
 from ..models.product_history import ProductHistory
-import api.alegra_management as alegra_api
-import api.route_management as route_api
-import api.purchase_management as purchase_api
-import api.inventory_management as inventory_api
-import api.ue_management as ue_api
+from . import alegra_management as alegra_api
+from . import route_management as route_api
+from . import purchase_management as purchase_api
+from . import inventory_management as inventory_api
+from . import ue_management as ue_api
+
 # APIs relacionadas (se importan dentro de las funciones para evitar ciclos)
 # from . import alegra_management, route_management, purchase_management, inventory_management
 
